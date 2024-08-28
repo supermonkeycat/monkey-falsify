2bc030738ec388690485d5948279901c3f3b5c59
supermonkeycat <76791164+supermonkeycat@users.noreply.github.com>
2024-08-28 19:31:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,49 @@ function switchStylesheet(newStylesheet) { //switches styles
         var link = document.getElementById('stylesheet');
         link.href = newStylesheet;
     }
+    // about blanker function
+function aboutblanker() {
+    var newWindow = window.open('about:blank');
+    if (newWindow) {
+        window.location.replace("https://www.google.com/webhp?igu=1");
+        var uURL = window.config.proxyLink
+        newWindow.document.open();
+        newWindow.document.write(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <style>
+                    html, body {
+                        margin: 0;
+                        padding: 0;
+                        overflow: hidden;
+                        height: 100%;
+                        width: 100%;
+                    }
+                    iframe {
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: 100%;
+                        height: 100%;
+                        border: none;
+                    }
+                </style>
+            </head>
+            <body>
+                <iframe src="${uURL}" frameborder="0"></iframe>
+            </body>
+            </html>
+        `);
+        newWindow.document.close();
+    } else {
+        console.error('Failed to open a new window.');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const secretCode = window.config.entryPhrase;
+    const alternatecode = window.config.blanker;
     let input = '';
 
     document.addEventListener('keydown', (event) => {
@@ -13,6 +54,10 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('guard').style.display = 'none'
             switchStylesheet('truth.css')
             document.getElementById('secretContent').style.display = 'block';
+        }
+// same as above but with the about blanker code and calls the about blanker function
+        if (input.endsWith(alternatecode)) {
+            aboutblanker();
         }
        //optional
         if (input.length > secretCode.length) {
