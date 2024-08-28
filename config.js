510a24eb1ee7f77b61fbfab8dbee23b9c2990c82
supermonkeycat <76791164+supermonkeycat@users.noreply.github.com>
2024-08-28 17:27:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.config = {
   entryPhrase: "letmein",  // phrase to enter the real site
-  proxyLink: "https://v4-production-4fc2.up.railway.app/" // proxy link
+  proxyLink: "surfdoge.pro" // proxy link, highly recommended you change it
 };
