e46588bfef32fb93f04a7b5075385060b0cdef25
supermonkeycat <76791164+supermonkeycat@users.noreply.github.com>
2024-09-03 17:59:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.config = {
   entryPhrase: "letmein",  // phrase to enter the real site
-  proxyLink: "surfdoge.pro", // proxy link, highly recommended you change it
+  proxyLink: "example.com", // proxy link, highly recommended you change it
   blanker: "abc123", // opens your proxy link in about blank
   leverofdoom: "no"
 };
