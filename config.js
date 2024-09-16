7f40275beec3d5f707f41c3fc399e4e9aa5c3e6e
supermonkeycat <76791164+supermonkeycat@users.noreply.github.com>
2024-09-16 13:06:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ window.config = {
   proxyLink: "https://example.com", // proxy link, highly recommended you change it
   blanker: "byebye", // opens your proxy link in about blank
   leverofdoom: "no",
-  alternativeproxy: "https://youtube.com" // alternative Proxy
+  alternativeproxy: "https://youtube.com", // alternative Proxy
+  useproxy: "true" // Some schools have certain restrictions on proxies that can cause students to get in massive trouble. Ticking this to false changes the website to only have games & movies
 };
