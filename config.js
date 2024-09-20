c617900c96d56b64aa30f06434f8a93f48c67c78
supermonkeycat <76791164+supermonkeycat@users.noreply.github.com>
2024-09-20 17:06:16 -0400

    v2 1/x

---
+++ config.js
@@ -0,0 +1,8 @@
+window.config = {
+  entryPhrase: "getmein",  // phrase to enter the real site
+  proxyLink: "https://example.com", // proxy link, highly recommended you change it
+  blanker: "byebye", // opens your proxy link in about blank
+  leverofdoom: "no",
+  alternativeproxy: "https://youtube.com", // alternative Proxy
+  useproxy: "false" // Some schools have certain restrictions on proxies that can cause students to get in massive trouble. Ticking this to false changes the website to only have games & movies (not in current use)
+};
